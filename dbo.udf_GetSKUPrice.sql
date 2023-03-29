963f9d02067be442a0e8fc07194f17b41e92579f
Lemernant <123646777+Lemernant@users.noreply.github.com>
2023-03-29 10:00:44 +0300

    Add files via upload

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -4,9 +4,15 @@ RETURNS decimal(18, 2)
 AS
 	BEGIN
 		DECLARE @val int;
+		DECLARE @qty int;
+		DECLARE @res numeric;
 		SELECT @val = p.Value
 		FROM dbo.Basket AS p
-		WHERE p.ID_SKU = @ID_SKU
-		RETURN @val
+		WHERE p.ID_SKU = @ID_SKU;
+		SELECT @qty = p.Quantity
+		FROM dbo.Basket AS p
+		WHERE p.ID_SKU = @ID_SKU;
+		SELECT @res = @val / @qty;
+		RETURN @res
 	END;
 GO
