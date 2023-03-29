1f9b7ecef2d4ebe8a3eac0313a0a0dee57f61249
Lemernant <123646777+Lemernant@users.noreply.github.com>
2023-03-29 10:02:36 +0300

    Add files via upload

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -6,10 +6,10 @@ AS
 		DECLARE @val int;
 		DECLARE @qty int;
 		DECLARE @res numeric;
-		SELECT @val = p.Value
+		SELECT @val = SUM(p.Value)
 		FROM dbo.Basket AS p
 		WHERE p.ID_SKU = @ID_SKU;
-		SELECT @qty = p.Quantity
+		SELECT @qty = SUM(p.Quantity)
 		FROM dbo.Basket AS p
 		WHERE p.ID_SKU = @ID_SKU;
 		SELECT @res = @val / @qty;
