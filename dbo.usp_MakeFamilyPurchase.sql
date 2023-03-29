801ba6537bdca4090efd8bca06bce0514bfa7f55
Lemernant <123646777+Lemernant@users.noreply.github.com>
2023-03-29 11:20:12 +0300

    Add files via upload

---
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR ALTER PROC dbo.MakeFamilyPurchase
+(@FamilySurName AS VARCHAR(255))
+AS
+DECLARE @price numeric;
+DECLARE @id int;
+SELECT @id = ID FROM dbo.Family
+WHERE SurName = @FamilySurName;
+SELECT @price = Value 
+FROM dbo.Basket
+WHERE ID_Family = @id;
+BEGIN TRY
+UPDATE dbo.Family
+SET BudgetValue = BudgetValue - SUM(@price)
+WHERE SurName = @FamilySurName;
+END TRY
+BEGIN CATCH
+PRINT 'Фамилия не найдена';
+END CATCH
+GO
